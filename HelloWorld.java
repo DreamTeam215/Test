d5996a481eeabaf5579e75a56e37f1f6d0b4802d
gryoung1945 <george.r.young@asu.edu>
2016-04-17 21:56:17 -0700

    Test
    Displays "Hello World!" on screen

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * Displays "Hello World!" on screen.
+ * 
+ * @author (George) 
+ * @version (04/17/2016)
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        
+    }
+
+}
